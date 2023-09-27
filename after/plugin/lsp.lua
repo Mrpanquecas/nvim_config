32d3e6b745d149e518ce7cd5063aa471dbebf09c
Ricardo Simões Costa <ricardo@ricardos-mbp.lan>
2023-09-27 12:08:20 +0100

    add auto format

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,10 +2,22 @@ local lsp_zero = require('lsp-zero')
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'rust_analyzer', 'gopls', 'html', 'lua_ls'},
-  handlers = {
-    lsp_zero.default_setup,
-  }
+    ensure_installed = {'tsserver', 'rust_analyzer', 'gopls', 'html', 'lua_ls'},
+    handlers = {
+        lsp_zero.default_setup,
+    }
+})
+
+lsp_zero.format_on_save({
+    format_opts = {
+        async = false,
+        timeout_ms = 10000,
+    },
+    servers = {
+        ['tsserver'] = {'javascript', 'javascriptreact', 'typescript', 'typescriptreact'},
+        ['html'] = {'html'},
+        ['rust_analyzer'] = {'rust'},
+    }
 })
 
 lsp_zero.on_attach(function(client, bufnr)
@@ -18,14 +30,14 @@ local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
     mapping = {
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-},
-  --formatting = {
-   -- fields = {'abbr', 'kind', 'menu'},
-    --format = require('lspkind').cmp_format({
-     -- mode = 'symbol', -- show only symbol annotations
-      --maxwidth = 50, -- prevent the popup from showing more than provided characters
-      --ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
-    --})
- -- }
-})
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
+    },
+    --formatting = {
+        -- fields = {'abbr', 'kind', 'menu'},
+        --format = require('lspkind').cmp_format({
+            -- mode = 'symbol', -- show only symbol annotations
+            --maxwidth = 50, -- prevent the popup from showing more than provided characters
+            --ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+            --})
+            -- }
+        })
